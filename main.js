49b7047b363c77c597eefd1ed89846f8e94a01bd
fernand0aguilar <fernando.aguilar@hotmail.com.br>
2021-04-25 04:34:46 +0000

    Deploying to gh-pages from  @ c3690794c05a3c5224fb73584c35f92843b7d5af 🚀

--- main.js
+++ main.js
@@ -226,7 +226,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
 const environment = {
     production: false,
-    MARVEL_BASE_URL: "http://gateway.marvel.com",
+    MARVEL_BASE_URL: "https://gateway.marvel.com",
     MARVEL_API_PUBLIC_KEY: "961ed5491a9fc99083c8cc3033feecbd",
     MARVEL_API_PRIVATE_KEY: "01731978868fa44628260bfe672dcc809846a537",
     GOOGLE_API_KEY: "AIzaSyDmTJRXzY-tTlJR7dwxjx7aZrcIKBB89mM"
